d8cf0ac211b394273e5bdb96705736bda2c55def
Tripathi <ashutosh.tripathi02@sap.com>
2017-08-13 16:23:14 +0530

    UI First Cut

--- src/Components/ShopItem.js
+++ src/Components/ShopItem.js
@@ -4,23 +4,29 @@ import React, {
 
 class ShopItem extends Component {
   render() {
-    return ( <
-      li className = "Shop" >
-      <
-      div >
-      <
-      strong > {
-        this.props.shop.shopName
-      } < /strong>  </div > < div > {
-        this.props.shop.shopType
-      } < /div>  <div> {
-      this.props.shop.area
-    } < /div>  <div> {
-    this.props.shop.description
-  } < /div>  <div> {
-  this.props.shop.landmark
-} < /div> < /
-li >
+    return (
+<div className="shop-container">
+      <div className ="shop">
+        <div className="shop-photo">
+          <img height = "150" width = "150" src = "http://www.thehindu.com/news/national/article17529553.ece/alternates/FREE_300/19THYOGI" / >
+        </div>
+        <div className="shop-detail">
+          <div><strong > {this.props.shop.shopName} </strong></div >
+
+          <div> { this.props.shop.mobile} </div>
+
+          <div className="shop-description"> { this.props.shop.description} </div>
+
+          <div> { this.props.shop.homeDelivery} </div>
+          <div> { this.props.shop.shopType } </div >
+
+          <div className="shop-address"> { this.props.shop.address} {this.props.shop.landmark} { this.props.shop.area }</div>
+
+          <div>{ this.props.shop.alternateMobile} { this.props.shop.thirdContact} </div>
+
+        </div>
+      </div>
+</div>
 );
 }
 }
